4166642ccddc00c9a2568a9f559a0b7ebba99f7d
lucax13 <lucas13lima2006@gmail.com>
2025-02-04 11:16:05 -0300

    exemplos

--- 04-objetos-genericos.php
+++ 04-objetos-genericos.php
@@ -12,10 +12,32 @@
 <?php
 /*criando uma instancia de uma classe generica chamada stdclass (stander class)*/
 $usuario = new stdClass(); //instancia ou objeto
+
+//adicionando propriedades/atributos ao objeto
+
+$usuario -> nome = "mickey mouse";
+$usuario -> idade = 19;
+$usuario -> email = "miscamusca@gmail.com";
+$usuario -> telefone = ["11 965417890"];
+
 ?> 
 
     <h2>analisando a estrutura do objeto</h2>
     <pre><?=var_dump($usuario)?></pre>
+    <hr>
+
+    <h2>saida de dados</h2>
+    <p>Nome: <?=$usuario->nome?></p>
+    <p>idade: <?=$usuario->idade?> Anos</p>
+    <p>Celular: <?=$usuario->telefone[0] ?></p>
+    <hr>
+
+    
+   
+
+
+
+
 
 
 
