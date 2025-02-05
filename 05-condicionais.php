2490a0d385f54af9d28741844f18c00c4b53d91b
lucax13 <lucas13lima2006@gmail.com>
2025-02-05 09:00:25 -0300

    add CSS

--- 05-condicionais.php
+++ 05-condicionais.php
@@ -5,6 +5,12 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Php condicionais</title>
 </head>
+<style>
+    .comprar{color: red;}
+    .urgente{color: red; background-color: yellow;}
+    .normal{color: green;}
+
+</style>
 <body>
     <h1>estrutura condicionais</h1>
     <hr>
@@ -25,20 +31,29 @@ if($numero > 1) echo "<p>$numero é maior que 1</p>";
     <h2>composta</h2>
 <?php
 $produto = "Iphone 13";
-$qtdEmEstoque = 10;
-$qtdCritica = 3;
+$qtdEmEstoque = 10; //o que temos
+$qtdCritica = 3; // minimo necessario
  
  echo "<h3>$produto</h3>";
  echo"<h4>unidades de estoque: $qtdEmEstoque </h4>";
 
  if($qtdEmEstoque < $qtdCritica){
-    echo"<p>é necessário comprar!</p>";
+    echo"<p class= comprar>é necessário comprar!</p>";
+
+    if($qtdEmEstoque === 0){
+     echo "<p class = urgente><strong>URGENTE</strong></p>"  ; 
+    }
+
  } else{
-    echo "<p>estoque normal</p>";
+    echo "<p class = normal>estoque normal</p>";
  }
 
-
 ?>
+
+
+
+
+<hr>
     <h2>encadeada</h2>
 
 </body>
