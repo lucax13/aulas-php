a8b553539c5ce73d268d824d47a7cc5875d0fafe
lucax13 <lucas13lima2006@gmail.com>
2025-02-05 10:00:20 -0300

    refatoração

--- 05-condicionais-versão2.php
+++ 05-condicionais-versão2.php
@@ -1,76 +1,96 @@
 <!DOCTYPE html>
 <html lang="pt-br">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Php condicionais</title>
 </head>
 <style>
-    .comprar{color: red;}
-    .urgente{color: red; background-color: yellow;}
-    .normal{color: green;}
+    .comprar {
+        color: red;
+    }
 
+    .urgente {
+        color: red;
+        background-color: yellow;
+    }
+
+    .normal {
+        color: green;
+    }
 </style>
+
 <body>
     <h1>Versão 2 estrutura condicionais</h1>
     <hr>
 
     <h2>simples usando <code>If</code></h2>
- 
-<?php
-$numero = 5;
-if($numero > 1){
-?>   
-    <p><?=$numero?> é maior que 1</p>
 
-<?php
-}
-?>
+    <?php
+    $numero = 5;
+    if ($numero > 1) {
+    ?>
+        <p><?= $numero ?> é maior que 1</p>
 
-<hr>
-    <h2>composta usando <code>if/else</code></h2>
-<?php
-$produto = "Iphone 13";
-$qtdEmEstoque = 10; //o que temos
-$qtdCritica = 3; // minimo necessario
- 
- echo "<h3>$produto</h3>";
- echo"<h4>unidades de estoque: $qtdEmEstoque </h4>";
-
- if($qtdEmEstoque < $qtdCritica){
-    echo"<p class= comprar>é necessário comprar!</p>";
-
-    if($qtdEmEstoque === 0){
-     echo "<p class = urgente><strong>URGENTE</strong></p>"  ; 
+    <?php
     }
+    ?>
 
- } else{
-    echo "<p class=\"normal\">estoque normal</p>";
- }
+    <hr>
+    <h2>composta usando <code>if/else</code></h2>
+    <?php
+    $produto = "Iphone 13";
+    $qtdEmEstoque = 10; //o que temos
+    $qtdCritica = 3; // minimo necessario
+    ?>
 
-?>
+    <h3><?= $produto ?></h3>
 
 
+    <h4>unidades de estoque: <?= $qtdEmEstoque ?> </h4>
 
+    <?php
+    if ($qtdEmEstoque < $qtdCritica) {
+    ?>
+        <p class=comprar>é necessário comprar!</p>
 
-<hr>
+        <?php
+        if ($qtdEmEstoque === 0) {
+        ?>
+            <p class=urgente><strong>URGENTE</strong></p>
+        <?php
+        }
+        ?>
+
+    <?php
+    } else {
+    ?>
+        <p class=normal> estoque normal</p>
+
+    <?php
+    }
+    ?>
+
+    <hr>
     <h2>encadeada usando <code>if/else/elseif</code></h2>
-<!--verifique a idade de uma pessoa e determine se ela é criança, adolecente, adulto ou idosa.-->
+    <!--verifique a idade de uma pessoa e determine se ela é criança, adolecente, adulto ou idosa.-->
 
-<?php
-$idade = 15;
-if ($idade <= 12){
-    echo "<p>Criança</p>"; 
-} elseif($idade <= 17){
-    echo "<p>Adolescente</p>";
-} elseif($idade <= 59){
-    echo "<p>adulto</p>";
-} else{
-    echo "<p>idoso</p>";
-}
-?>
+    <?php
+    $idade = 15;
+    if ($idade <= 12) {
+        echo "<p>Criança</p>";
+    } elseif ($idade <= 17) {
+        echo "<p>Adolescente</p>";
+    } elseif ($idade <= 59) {
+        echo "<p>adulto</p>";
+    } else {
+        echo "<p>idoso</p>";
+    }
+    ?>
 
 
 
 </body>
+
 </html>
