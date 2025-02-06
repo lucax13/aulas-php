76e85dc29a7c33aeeccc2e3c304753e18cf7bf18
lucax13 <lucas13lima2006@gmail.com>
2025-02-06 08:25:14 -0300

    operadores logicos

---
+++ 06-operadores-logicos.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP - operadores logicos</title>
+</head>
+<body>
+    <h1>relembrando operadores logicos</h1>
+    <hr>
+
+    <h2>&& (E)</h2>
+
+    <h2>|| (OU/OR)</h2>
+
+    <h2>! (não/not)</h2>
+    
+</body>
+</html>
