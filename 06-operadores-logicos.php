6e18231e9a21255b4f3c7b0cd5f11ca2856d7247
lucax13 <lucas13lima2006@gmail.com>
2025-02-06 09:01:14 -0300

    usando 3 operadores

--- 06-operadores-logicos.php
+++ 06-operadores-logicos.php
@@ -21,13 +21,53 @@ if($media >= 7 && $faltas <= 10) {
     echo "<p>Reprovado!</p>";
 }
 ?>
-
-
-
-
+<hr>
     <h2>|| (OU/OR)</h2>
+    <p><i>Basta que apenas uma condição seja <b>verdadeira/true</b></i></p>
+
+<?php
+$clienteVIP = false;
+$temCupom = true;
+
+if($clienteVIP || $temCupom) {
+    echo "<p>Desconto aplicado!</p>";
+} else {
+    echo "<p>Sem Desconto!</p>";
+}
+
+?>
+<hr>
 
     <h2>! (não/not)</h2>
-    
+    <p><i>É uma inversão de logica: verdadeiro<b>Falso</b>, Falso vira <b>verdadeira</b></i></p>
+
+<?php
+$usuarioLogado = false;
+
+if(!$usuarioLogado){
+    echo"<a href=\"login.php\">Login<a>";
+}else{
+    echo"<span>Bem vindo ao sistema</span>";
+}
+?>
+<hr>
+
+    <h2>usando os 3 operadores combinados para uma logica mais elaboradora</h2>
+<?php
+
+$idade = 16;
+$acompanhadoPais = true;
+$estaBebado = false;
+
+
+if($idade >= 18 || $acompanhadoPais  && !$estaBebado) {
+    echo"<p>Entrada permitida</p>";
+} else {
+    echo"<p>Entrada negada!</p>";
+}
+
+
+?>
+
 </body>
 </html>
