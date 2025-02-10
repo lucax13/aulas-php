d56d01aa5ffa0cce3b7a6d0d3b362caf802f2ed9
lucax13 <lucas13lima2006@gmail.com>
2025-02-10 11:43:55 -0300

    funçao

--- 08-funcoes.php
+++ 08-funcoes.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>PHP modelo</title>
+    <title>PHP Funções</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
     integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
 
@@ -11,7 +11,7 @@
 </head>
 <body>
     <div class="container">
-        <h1></h1>
+        <h1>Trabalhando com Funções</h1>
         <hr>
     </div>
 
