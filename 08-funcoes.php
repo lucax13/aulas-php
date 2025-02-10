4801a009b2d61967e9051657e19a3830d685690c
lucax13 <lucas13lima2006@gmail.com>
2025-02-10 11:52:25 -0300

    Update 08-funcoes.php

--- 08-funcoes.php
+++ 08-funcoes.php
@@ -12,9 +12,20 @@
 <body>
     <div class="container">
         <h1>Trabalhando com Funções</h1>
+
+        <h2>função como procedimento (ou sub-rotina)</h2>
+        <p><i>Procedimento <b>Não retornam</b>Nada.</i></p>
+
+<?php
+function exibirDadosDoAutor(){
+    echo "<h3>fulano de tal</h3>";
+    echo "<p><time>".date("d/m/Y")."</time></p>";
+}?>
+
+    <h3>chamada do procedimento</h3>
+    <div><?= exibirDadosDoAutor()?></div>
         <hr>
     </div>
-
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
 integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz"
  crossorigin="anonymous"></script>
