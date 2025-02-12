af91e088e14bda77d64a9e1d238841e134ebe5d9
lucax13 <lucas13lima2006@gmail.com>
2025-02-12 11:22:31 -0300

    correção

--- exercicios/exercicio05.php
+++ exercicios/exercicio05.php
@@ -8,49 +8,58 @@
     integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
 
 
-</head>
+    </head>
 <body>
     <div class="container">
         <h1>Exercicio Funções</h1>
         <hr>
+
 <?php
-$nota = 10;
-$nota1 = 8;
-$nota2 = 3;
-
- $media = calculoMedia($nota, $nota1, $nota2);
-
-    if($media >= 7 ){
- ?> 
-        <p>Sua Nota é: <?=$media?> <span class="badge text-bg-primary">Aprovado</span></p>
-<?php
-} else { ?>
-    <p>Sua Nota é: <?=$media?> <span class="badge text-bg-danger">Reprovado</span></p>
-<?php } ?>
-
-    <hr>
-
-
-<?php 
-function calculoMedia($notas, $notas1, $notas2){
-    
-   $ultimaNota = ($notas + $notas1 + $notas2) / 3;
-    return  $ultimaNota;
+function calculoMedia($valor1, $valor2, $valor3){
+    return ($valor1 + $valor2 + $valor3) / 3;
 }
 
- $resultado = calculoMedia(1 , 2, 5);
+$nota1 = 10;
+$nota2 = 8;
+$nota3 = 3;
 
- 
+$media = calculoMedia($nota1, $nota2, $nota3);
 
-if($resultado >= 7 ){
-    ?> 
-           <p>Sua Nota é: <?=$resultado?> <span class="badge text-bg-primary">Aprovado</span></p>
-   <?php
-   } else { ?>
-       <p>Sua Nota é: <?=$resultado?> <span class="badge text-bg-danger">Reprovado</span></p>
-   <?php } ?>
+if($media >= 7) { 
+?> 
+        <p>Sua Nota é: <?=$media?> <span class="badge text-bg-primary">Aprovado</span></p>
+<?php
+} else { 
+?> 
+        <p>Sua Nota é: <?=$media?> <span class="badge text-bg-danger">Reprovado</span></p>
+<?php 
+}
+?>
 
-    </div>
+        <hr>
+
+<?php 
+function exibeResultado($valor1, $valor2, $valor3){
+    $ultimaNota = ($valor1 + $valor2 + $valor3) / 3;
+
+    if($ultimaNota >= 7) {
+        $status = "Aprovado";
+        $badgeClass = "text-bg-primary";
+    } else {
+        $status = "Reprovado";
+        $badgeClass = "text-bg-danger";
+    }
+
+    return "<p>Sua Nota é: $ultimaNota <span class='badge $badgeClass'>$status</span></p>";
+}
+
+$resultado = exibeResultado(8, 10, 6);
+echo $resultado;
+?>
+
+    </div> 
+</body>
+
 
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
 integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz"
