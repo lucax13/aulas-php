b8fa782dc18898025b92e833e982e6cf8bb1a360
lucax13 <lucas13lima2006@gmail.com>
2025-02-13 11:40:19 -0300

    incluindo fragmento de HTML

--- 10-include-e-require.php
+++ 10-include-e-require.php
@@ -25,7 +25,20 @@
         <hr>
 
         <p>O aluno Fulano de tal tem 20 anos e é <?=verificarIdade(20)?> de idade</p>
+    <hr>
+
+    <article>
+        <h2>Título qualquer...</h2>
+        
+        <!-- incluindo fragmento de HTML -->
+        <?php include "textos.html"; ?>
+    </article>
+    
+    
+    
     </div>
+
+
 
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
 integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz"
