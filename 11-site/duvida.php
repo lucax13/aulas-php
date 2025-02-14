588d04121a26e00720c51368edd2d775b4724599
lucax13 <lucas13lima2006@gmail.com>
2025-02-14 08:37:00 -0300

    Create duvida.php

---
+++ 11-site/duvida.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Duvidas - Site com PHP</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
+    integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+
+
+</head>
+<body>
+    <div class="container">
+        <header>
+            <h1>Curso - Site com PHP</h1>
+            <nav>
+                <a href="index.php">Home</a>
+                <a href="curso.php">Cursos</a>
+                <a href="duvida.php">Dúvidas</a>
+            </nav>
+        </header>
+        <hr>
+
+        <main>
+            <h2>Pagina de Duvidas</h2>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus voluptate doloremque doloribus sequi distinctio, sunt non? Et obcaecati vero optio labore eveniet, molestiae corrupti dolore itaque voluptatibus culpa sit error!</p>
+            
+            <h3>Principais Dúvidas</h3>
+            <details>
+                <summary>Como se cadastrar?</summary>
+                <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ab fugit perferendis, eos eaque aut placeat corporis aliquam molestiae dolor laborum qui cumque, error, ipsa soluta unde numquam quia debitis repudiandae?</p>
+            </details>
+            <details>
+                <summary>Os cursos são gratuitos?</summary>
+                <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ab fugit perferendis, eos eaque aut placeat corporis aliquam molestiae dolor laborum qui cumque, error, ipsa soluta unde numquam quia debitis repudiandae?</p>
+            </details>
+        </main>
+        
+        <footer>
+            <p>Site criado para curso Tec. informatica web</p>
+        </footer>
+    </div>
+
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
+integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz"
+ crossorigin="anonymous"></script>
+</body>
+</html>
