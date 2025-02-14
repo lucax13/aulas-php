c788b240f9507ecf90abf0724086891cc6607c30
lucax13 <lucas13lima2006@gmail.com>
2025-02-14 08:33:10 -0300

    Create curso.php

---
+++ 11-site/curso.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Site com PHP</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
+    integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+
+
+</head>
+<body>
+    <div class="container">
+        <header>
+            <h1>Curso - Site com PHP</h1>
+            <nav>
+                <a href="index.php">Home</a>
+                <a href="curso.php">Cursos</a>
+                <a href="duvida.php">Dúvidas</a>
+            </nav>
+        </header>
+        <hr>
+
+        <main>
+            <h2>Pagina de cursos</h2>
+            <p>veja os cursos disponiveis:</p>
+            <ul>
+                <li>Culinaria</li>
+                <li>Direção Defensiva</li>
+                <li>Como escapar de um terremoto</li>
+            </ul>
+            
+        </main>
+        
+        <footer>
+            <p>Site criado para curso Tec. informatica web</p>
+        </footer>
+    </div>
+
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
+integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz"
+ crossorigin="anonymous"></script>
+</body>
+</html>
