0e90bb8697728afb179cb8f1b5e56bc795b4af3b
lucax13 <lucas13lima2006@gmail.com>
2025-02-14 10:34:41 -0300

    pagina contato

---
+++ 11-site/contato.php
@@ -0,0 +1,18 @@
+<?php include "includes/cabecalho.php";?>
+
+
+        <h2>Pagina de contato</h2>
+        <p>entre em contato conosco:</p>
+         
+    <div class="mb-3">
+  <label for="formGroupExampleInput" class="form-label">Nome:</label>
+  <input type="text" class="form-control" id="formGroupExampleInput" placeholder="nome">
+</div>
+<div class="mb-3">
+  <label for="formGroupExampleInput2" class="form-label">Email:</label>
+  <input type="text" class="form-control" id="formGroupExampleInput2" placeholder="adicione um email ">
+</div>
+
+ <button type="button" class="btn btn-primary">enviar</button>
+
+<?php include "includes/rodape.php";?>
--- 11-site/includes/cabecalho.php
+++ 11-site/includes/cabecalho.php
@@ -1,9 +1,16 @@
+<?php 
+$titulo = "Site com PHP";
+
+
+?>
+
+
 <!DOCTYPE html>
 <html lang="pt-br">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Site com PHP</title>
+    <title><?=$titulo?></title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
     integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
 
@@ -17,6 +24,7 @@
                 <a href="index.php">Home</a>
                 <a href="curso.php">Treinamentos</a>
                 <a href="duvida.php">Dúvidas</a>
+                <a href="contato.php">Contato</a>
             </nav>
         </header>
         <hr>
