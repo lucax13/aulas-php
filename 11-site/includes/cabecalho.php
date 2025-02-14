18a7e15494e9f5d2da87a1e00444031591bad178
lucax13 <lucas13lima2006@gmail.com>
2025-02-14 11:06:09 -0300

    Update cabecalho.php

--- 11-site/includes/cabecalho.php
+++ 11-site/includes/cabecalho.php
@@ -1,16 +1,30 @@
 <?php 
-$titulo = "Site com PHP";
+$pagina = basename($_SERVER["PHP_SELF"]);
 
-
+switch ($pagina){
+    case 'index.php':
+        $titulo = "Pagina Inicial";
+        break;
+    case 'curso.php';
+        $titulo = "Treinamentos";
+        break;
+    case 'duvida.php';
+        $titulo = "Dúvidas";
+        break;
+    case 'contato.php';
+        $titulo = "contato";
+        break;   
+}
 ?>
 
 
 <!DOCTYPE html>
 <html lang="pt-br">
 <head>
+
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title><?=$titulo?></title>
+    <title> <?=$titulo?> - Site com PHP</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
     integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
 
